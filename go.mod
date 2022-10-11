a9d9c8fbef2c3d8709fc7e7e682817523659729b
David <dchung0330@gmail.com>
2022-10-10 22:14:42 -0400

    created slack bot that uploads a local file to slack channel.

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/dchung117/slackbot_file_golang
+
+go 1.19
+
+require github.com/slack-go/slack v0.11.3
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/dchung117/slackbot_file_golang/secret"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	// set bot token and channel id environment variable
+	os.Setenv("SLACK_BOT_TOKEN", secret.SLACK_BOT_TOKEN)
+	os.Setenv("CHANNEL_ID", secret.CHANNEL_ID)
+
+	// create api client connection to slack
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+
+	// set channel array
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+
+	// set file array (place path to file here)
+	fileArr := []string{"/Users/davidchung/Downloads/wine-reviews.csv"}
+
+	// loop through files and upload
+	for i := 0; i < len(fileArr); i++ {
+		// set upload parameters
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+
+		// upload file to channel
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+
+		// print out file details
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URLPrivate)
+	}
+}
